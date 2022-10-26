a38204ad999569680b3c2870eeb2b72feb626bf4
thatdavidma <116685223+thatdavidma@users.noreply.github.com>
2022-10-25 20:25:12 -0400

    Adding env file due to it being ignored

---
+++ src/env.js
@@ -0,0 +1,4 @@
+export default {
+    GRAPHQL_ENDPOINT: 'https://dn6jtahorvekvabij3ziq3rsk4.appsync-api.us-east-1.amazonaws.com/graphql',
+    GRAPHQL_API_KEY: 'da2-jfxhcmqg6jhgjjzdca2w2d5o7m'
+};  
